4c8241ee6ac2ce41e3d4a3baaf49d3dd23f5d6c0
Joseph Phelan <jphelan@widen.com>
2025-03-24 12:27:57 -0500

    add text card section

---
+++ src/components/textAndCardSection.jsx
@@ -0,0 +1,65 @@
+import { cva } from 'class-variance-authority'
+
+import { cn } from '../lib/utils'
+import Button from './button'
+import Heading from './heading'
+
+const gridVariants = cva('mx-auto my-16 grid place-items-center gap-8', {
+  variants: {
+    cardLayout: {
+      '2 columns': 'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2',
+      '3 columns': 'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3',
+      '4 columns': 'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4',
+      '6 columns': 'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-6',
+    },
+  },
+  defaultVariants: {
+    cardLayout: '3 columns',
+  },
+})
+
+const layoutVariants = cva('flex flex-col flex-wrap items-center gap-4', {
+  variants: {
+    layout: {
+      row: 'flex-row',
+      column: 'flex-col',
+    },
+  },
+  defaultVariants: {
+    layout: 'row',
+  },
+})
+
+const TextAndCardSection = ({
+  cardLayout = '3 columns',
+  layout = 'row',
+  preHeading = 'Our customers',
+  heading = `You're in good company.`,
+  headingElement = 'h2',
+  text = 'Use this section to introduce the organizations you work with and build trust in your organization through association.',
+  children,
+  linkLabel = 'Read our customer stories',
+  link,
+}) => {
+  return (
+    <div className={layoutVariants({ layout })}>
+      <div className="mx-auto flex max-w-2xl flex-col gap-6">
+        <Heading
+          preHeading={preHeading}
+          heading={heading}
+          headingSize="Medium"
+          headingElement={headingElement}
+        />
+        <p>{text}</p>
+        <div>
+          <Button link={link} variant="outlineDark">
+            {linkLabel}
+          </Button>
+        </div>
+      </div>
+      <div className={cn(gridVariants({ cardLayout }))}>{children}</div>
+    </div>
+  )
+}
+
+export default TextAndCardSection
---
+++ src/components/textAndCardSection.stories.jsx
@@ -0,0 +1,71 @@
+import CardContainer from './cardContainer.jsx'
+import LogoCard from './logo-card.jsx'
+import TextAndCardSection from './textAndCardSection.jsx'
+
+const meta = {
+  title: 'Sections/textAndCardSection',
+  component: TextAndCardSection,
+  argTypes: {
+    layout: {
+      options: ['row', 'column'],
+      control: { type: 'select' },
+    },
+    headingElement: {
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      control: { type: 'select' },
+    },
+    headingSize: {
+      options: ['Large', 'Medium', 'Small'],
+      control: { type: 'select' },
+    },
+    text: String,
+    textColor: {
+      options: ['Dark', 'Light'],
+      control: { type: 'select' },
+    },
+    cardLayout: {
+      options: ['2 columns', '3 columns', '4 columns', '6 columns'],
+      control: { type: 'select' },
+    },
+    previewCardType: {
+      options: ['Default Card', 'Feature Card'],
+      control: { type: 'select' },
+    },
+    previewCardCount: {
+      options: [1, 2, 3, 4, 6, 8, 12, 16],
+      control: { type: 'select' },
+    },
+  },
+}
+export default meta
+
+export const TitleWith3ColLogoCards = {
+  args: {
+    layout: 'row',
+    preHeading: 'Our customers',
+    heading: "You're in good company.",
+    headingElement: 'h2',
+    headingSize: 'Medium',
+    textColor: 'Dark',
+    text: 'Use this section to introduce the organizations you work with and build trust in your organization through association.',
+    linkLabel: 'Read our customer stories',
+    cardLayout: '3 columns',
+    previewCardType: 'Feature Card',
+    previewCardCount: 6,
+  },
+  render: (args) => {
+    // Create an array with a length equal to previewCardCount and fill it with null
+    const cardsArray = Array(args.previewCardCount).fill(null)
+    const cards = cardsArray.map((_, index) => {
+      return (
+        <LogoCard
+          key={`logo-card-${index}`}
+          image="/src/assets/images/logo.svg"
+          altText="Company logo"
+        />
+      )
+    })
+
+    return <TextAndCardSection {...args}>{cards}</TextAndCardSection>
+  },
+}
