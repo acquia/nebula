4d99261588f8e0f65c56154235175a408486238e
Anna Vo <477524+avo@users.noreply.github.com>
2025-03-26 12:24:48 -0500

    Additional footer examples (#9)

--- src/components/Footer/footer.stories.jsx
+++ src/components/Footer/footer.stories.jsx
@@ -1,13 +1,63 @@
+import drupalLogo from '../../assets/images/logo.svg'
+import Branding from '../branding.jsx'
+import Copyright from './copyright'
 import Footer from './footer'
+import FooterCopyright from './footer-copyright'
+import FooterLogoTop from './footer-logo-top'
+import FooterMinimal from './footer-minimal'
+import Social from './social'
 
 const meta = {
   component: Footer,
   tags: ['autodocs'],
   title: 'Components/Footer',
 }
+export default meta
+
+export const CopyrightOnly = {
+  render: (args) => <Copyright {...args} />,
+  args: {
+    text: `© ${new Date().getFullYear()} Company Name.`,
+  },
+}
+
+export const SocialOnly = {
+  render: (args) => <Social {...args} />,
+  args: {},
+}
+
+export const FooterWithCopyright = {
+  render: (args) => <FooterCopyright {...args} />,
+  args: {},
+}
+
+export const Minimal = {
+  render: (args) => <FooterMinimal {...args} />,
+  args: {},
+}
 
 export const Default = {
   args: {},
 }
 
-export default meta
+export const FooterTop = {
+  render: () => {
+    const logo = <Branding homeUrl="#" logo={drupalLogo} />
+    return <FooterLogoTop logo={logo} />
+  },
+  args: {},
+}
+
+export const FullFooter = {
+  render: () => {
+    const logo = <Branding homeUrl="#" logo={drupalLogo} />
+    return (
+      <footer>
+        <FooterLogoTop footerElement="div" logo={logo} />
+        <Footer footerElement="div" />
+        <FooterCopyright footerElement="div" />
+      </footer>
+    )
+  },
+  args: {},
+}
