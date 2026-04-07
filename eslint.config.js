e72aa4710b70352560295972a6e7c8f595bd860c
Balint Kleri <b@balintbrews.com>
2026-04-07 10:54:47 +0200

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,14 @@ import { recommended } from "@drupal-canvas/eslint-config";
 export default [
   ...recommended,
   {
+    files: ["scripts/**/*.js"],
+    languageOptions: {
+      globals: {
+        process: "readonly",
+      },
+    },
+  },
+  {
     ignores: ["dist/**"],
   },
 ];
